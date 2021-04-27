2eb2c04b49bbab79a04773b089445b4be00899e0
Cole Brooks <cole.brooks@hotmail.com>
2021-04-27 17:33:29 -0500

    initial commit

---
+++ CryptoTracker/CryptoTracker/main.c
@@ -0,0 +1,17 @@
+/*
+ * CryptoTracker.c
+ *
+ * Created: 4/27/2021 5:32:56 PM
+ * Author : Cole Brooks, Thomas Butler
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
+
